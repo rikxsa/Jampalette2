acf0a670eb8ef75a663d14bd244c92f64933e310
rikxsa <sakuramaru908@gmail.comgit config --global user.email sakuramaru908@gmail.com>
2015-06-24 02:37:46 +0900

    firstcommit

---
+++ JAM/open.swift
@@ -0,0 +1,19 @@
+//
+//  open.swift
+//  JAM
+//
+//  Created by 桜木力丸 on 2015/06/18.
+//  Copyright (c) 2015年 桜木力丸. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerA: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .blueColor()
+    }
+}
+
---
+++ JAMTests/JAMTests.swift
@@ -0,0 +1,36 @@
+//
+//  JAMTests.swift
+//  JAMTests
+//
+//  Created by 桜木力丸 on 2015/06/16.
+//  Copyright (c) 2015年 桜木力丸. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class JAMTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
