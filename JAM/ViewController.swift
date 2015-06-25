642ab800eeaa0178e0c083c21029fdabd2be522e
rikxsa <sakuramaru908@gmail.comgit config --global user.email sakuramaru908@gmail.com>
2015-06-25 12:43:29 +0900

    secondcommit

--- JAM/ViewController.swift
+++ JAM/ViewController.swift
@@ -14,6 +14,15 @@ import AVFoundation
 
 
 
+
+
+
+
+
+
+
+
+
 //AudioPlayerDelegateプロトコル：音楽の再生／終了の処理
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
@@ -26,53 +35,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         let soundFilePath : NSString =
         NSBundle.mainBundle().pathForResource("C", ofType: "wav")!
         let fileURL : NSURL = NSURL(fileURLWithPath: soundFilePath as String)!
-        
-        
-        
-        //再生する音源のURLを生成(D)
-        let soundFilePathD : NSString =
-        NSBundle.mainBundle().pathForResource("D", ofType: "wav")!
-        let fileURLD : NSURL = NSURL(fileURLWithPath: soundFilePathD as String)!
-        
-        //再生する音源のURLを生成(E)
-        let soundFilePathE : NSString =
-        NSBundle.mainBundle().pathForResource("E", ofType: "wav")!
-        let fileURLE : NSURL = NSURL(fileURLWithPath: soundFilePathE as String)!
-        
-        //再生する音源のURLを生成(F)
-        let soundFilePathF : NSString =
-        NSBundle.mainBundle().pathForResource("F", ofType: "wav")!
-        let fileURLF : NSURL = NSURL(fileURLWithPath: soundFilePathF as String)!
-        
-        //再生する音源のURLを生成(G)
-        let soundFilePathG : NSString =
-        NSBundle.mainBundle().pathForResource("G", ofType: "wav")!
-        let fileURLG : NSURL = NSURL(fileURLWithPath: soundFilePathG as String)!
-        
-        //再生する音源のURLを生成(Ad)
-        let soundFilePathA : NSString =
-        NSBundle.mainBundle().pathForResource("Ad", ofType: "wav")!
-        let fileURLA : NSURL = NSURL(fileURLWithPath: soundFilePathA as String)!
-        
-        //再生する音源のURLを生成(Bd)
-        let soundFilePathB : NSString =
-        NSBundle.mainBundle().pathForResource("Bd", ofType: "wav")!
-        let fileURLB : NSURL = NSURL(fileURLWithPath: soundFilePathB as String)!
-        
-        //再生する音源のURLを生成(Ed)
-        let soundFilePathEd : NSString =
-        NSBundle.mainBundle().pathForResource("Ed", ofType: "wav")!
-        let fileURLEd : NSURL = NSURL(fileURLWithPath: soundFilePathEd as String)!
-    
-        //再生する音源のURLを生成(Fd)
-        let soundFilePathFd : NSString =
-        NSBundle.mainBundle().pathForResource("Fd", ofType: "wav")!
-        let fileURLFd : NSURL = NSURL(fileURLWithPath: soundFilePathFd as String)!
-    
-        //再生する音源のURLを生成(Gd)
-        let soundFilePathGd : NSString =
-        NSBundle.mainBundle().pathForResource("Gd", ofType: "wav")!
-        let fileURLGd : NSURL = NSURL(fileURLWithPath: soundFilePathGd as String)!
+            
         
         //AVAudioPlayerのインスタンス化
         audioPlayer = AVAudioPlayer(contentsOfURL: fileURL, error: nil)
@@ -83,6 +46,55 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     }
     
     
+
+    
+    
+//AudioPlayerDelegateプロトコル：音楽の再生／終了の処理
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+        
+        var audioPlayerD : AVAudioPlayer!
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+        
+            
+            //再生する音源のURLを生成(D)
+            let soundFilePathD : NSString =
+            NSBundle.mainBundle().pathForResource("D", ofType: "wav")!
+            let fileURLD : NSURL = NSURL(fileURLWithPath: soundFilePathD as String)!
+            
+            
+            
+            //AVAudioPlayerのインスタンス化
+            audioPlayerD = AVAudioPlayer(contentsOfURL: fileURLD, error: nil)
+            
+
+            
+            }
+ 
+    
+    
+            // Do any additional setup after loading the view, typically from a nib.
+            
+        }
+        
+
+    
+    
+ 
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
@@ -90,20 +102,37 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         
     }
     
+    
+    
     //ボタンをタップしている間音がなるアクション
     @IBAction func playButton(){
         audioPlayer.play()
         
-   
-    }
+   }
     
     //ボタンをタップをやめると音が止まるアクション
     @IBAction func playButton2(){
-            audioPlayer.stop()
-        }
+        audioPlayer.stop()
+    }
+    
+    
+    
+    @IBAction func playButtonDstart(){
+        audioPlayerD.play()
+    }
+    
+    
+    @IBAction func playButtonDstop(){
+        audioPlayerD.stop()
+    }
+
+    
+    
+ 
+
+
+  
           
     
-    
-    
-
+        
 }
